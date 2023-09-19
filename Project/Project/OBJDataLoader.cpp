3a683b33a1d13aa4040b05cc9d396ed37adeaee2
Artemis K. Frost (she/her) <github@artemiskfrost.com>
2023-09-19 06:37:12 -0600

    extract helper functions and data containers from LoadOBJMesh()

--- Project/Project/OBJDataLoader.cpp
+++ Project/Project/OBJDataLoader.cpp
@@ -8,12 +8,19 @@
 using std::vector;
 
 
-struct OBJTriangle
+struct UnstructuredVertex
 {
-	uint3 VertexIndices[3];
+	unsigned int PositionIndex;
+	unsigned int TexelIndex;
+	unsigned int NormalIndex;
 };
 
-struct RawMeshData
+struct OBJTriangle
+{
+	UnstructuredVertex Vertices[3];
+};
+
+struct UnstructuredMeshData
 {
 	vector<float3> Positions;
 	vector<float3> Texels;
@@ -21,46 +28,116 @@ struct RawMeshData
 	vector<OBJTriangle> Faces;
 };
 
-
-
-
-
-OBJMesh OBJMeshLoader::LoadOBJMesh(const char* filePath)
+struct CompactifiedMeshData
 {
-	RawMeshData rawMeshData = {};
+	vector<UnstructuredVertex> Vertices;
+	vector<unsigned int> Indices;
+};
+
+struct NormalizedMeshData
+{
+	OBJVertex* Vertices;
+	unsigned int* Indices;
+};
+
+
+CompactifiedMeshData CompactifyUnstructuredMeshData(UnstructuredMeshData unstructuredMeshData)
+{
+	CompactifiedMeshData result = {};
+	for (unsigned int faceIndex = 0; faceIndex < unstructuredMeshData.Faces.size(); faceIndex++)
+	{
+		for (unsigned int faceVertexIndex = 0; faceVertexIndex < 3; faceVertexIndex++)
+		{
+			bool isUnique = true;
+			unsigned int index = 0;
+			for (unsigned int unsortedVertexIndex = 0; unsortedVertexIndex < result.Vertices.size(); ++unsortedVertexIndex)
+			{
+
+				if (unstructuredMeshData.Faces[faceIndex].Vertices[faceVertexIndex].PositionIndex == result.Vertices[unsortedVertexIndex].PositionIndex
+					&& unstructuredMeshData.Faces[faceIndex].Vertices[faceVertexIndex].TexelIndex == result.Vertices[unsortedVertexIndex].TexelIndex
+					&& unstructuredMeshData.Faces[faceIndex].Vertices[faceVertexIndex].NormalIndex == result.Vertices[unsortedVertexIndex].NormalIndex)
+				{
+					isUnique = false;
+					index = unsortedVertexIndex;
+					break;
+				}
+			}
+
+			if (isUnique)
+			{
+				index = result.Vertices.size();
+				result.Vertices.push_back(unstructuredMeshData.Faces[faceIndex].Vertices[faceVertexIndex]);
+			}
+
+			result.Indices.push_back(index);
+		}
+	}
+
+	return result;
+}
+
+NormalizedMeshData NormalizeStructuredMeshData(UnstructuredMeshData unstructuredMeshData, CompactifiedMeshData compactifiedMeshData)
+{
+	NormalizedMeshData result = {};
+
+	result.Vertices = new OBJVertex[compactifiedMeshData.Vertices.size()];
+	for (unsigned int i = 0; i < compactifiedMeshData.Vertices.size(); ++i)
+	{
+		OBJVertex vertex = {};
+		vertex.Position.x = unstructuredMeshData.Positions[compactifiedMeshData.Vertices[i].PositionIndex].x;
+		vertex.Position.y = unstructuredMeshData.Positions[compactifiedMeshData.Vertices[i].PositionIndex].y;
+		vertex.Position.z = unstructuredMeshData.Positions[compactifiedMeshData.Vertices[i].PositionIndex].z;
+		vertex.Texel.x = unstructuredMeshData.Texels[compactifiedMeshData.Vertices[i].TexelIndex].x;
+		vertex.Texel.y = unstructuredMeshData.Texels[compactifiedMeshData.Vertices[i].TexelIndex].y;
+		vertex.Texel.z = unstructuredMeshData.Texels[compactifiedMeshData.Vertices[i].TexelIndex].z;
+		vertex.Normal.x = unstructuredMeshData.Normals[compactifiedMeshData.Vertices[i].NormalIndex].x;
+		vertex.Normal.y = unstructuredMeshData.Normals[compactifiedMeshData.Vertices[i].NormalIndex].y;
+		vertex.Normal.z = unstructuredMeshData.Normals[compactifiedMeshData.Vertices[i].NormalIndex].z;
+		result.Vertices[i] = vertex;
+	}
+
+	result.Indices = new unsigned int[compactifiedMeshData.Indices.size()];
+	for (unsigned int i = 0; i < compactifiedMeshData.Indices.size(); ++i)
+	{
+		result.Indices[i] = compactifiedMeshData.Indices[i];
+	}
+
+	return result;
+}
+
+UnstructuredMeshData ReadUnstructuredMeshDataFromFile(const char* filePath)
+{
+	UnstructuredMeshData result = {};
+
 	std::ifstream inputFileStream(filePath, std::ios::in);
-	char readLineInto[100];
-
-	// ----- PROCESS RAW DATA INTO LISTS -----
-
-	// verify that file opened
 	if (!inputFileStream.is_open())
 	{
 		_RPTN(0, "Could not open OBJ file to load data\n", NULL);
 	}
-	// read data from file
+
+	char readLineInto[100];
 	while (true)
 	{
-		// read next line
 		inputFileStream.getline(readLineInto, 100, '\n');
 		if (inputFileStream.eof())
 		{
 			break;
 		}
-		// copy line into separate string for strtok
+
+		// copy line into separate string to use strtok on it
 		char extractVerticesFrom[100];
 		strcpy_s(extractVerticesFrom, readLineInto);
 		char separators[] = " ";
-		char *token, *nextToken;
-		std::vector<char*> tokens;
-		token = strtok_s(extractVerticesFrom, separators, &nextToken);
+		char* nextToken = nullptr;
+		char* token = strtok_s(extractVerticesFrom, separators, &nextToken);
+
 		// check if line contains vertex data
+		vector<char*> tokens;
 		switch (readLineInto[0])
 		{
 			case 'v': //vertex
 			{
-				// split line into tokens
-				// first token is line identifier and is ignored
+				// split line into tokens; first token is line identifier and is ignored
 				while (token != NULL)
 				{
 					tokens.push_back(token);
@@ -70,34 +147,41 @@ OBJMesh OBJMeshLoader::LoadOBJMesh(const char* filePath)
 				// convert tokens into vertex data
 				switch (tokens[0][1])
 				{
-					case 'n': // normal
+					case 'n':
 						float3 normal;
 						normal.x = strtof(tokens[1], nullptr);
 						normal.y = strtof(tokens[2], nullptr);
 						normal.z = strtof(tokens[3], nullptr);
-						rawMeshData.Normals.push_back(normal);
+						result.Normals.push_back(normal);
 						break;
-					case 't': // texel
+
+					case 't':
 						float3 texel;
 						texel.x = strtof(tokens[1], nullptr);
 						texel.y = strtof(tokens[2], nullptr);
 						if (tokens.size() > 3)
+						{
 							texel.z = strtof(tokens[3], nullptr);
+						}
 						else
+						{
 							texel.z = 0.0f;
-						rawMeshData.Texels.push_back(texel);
+						}
+						result.Texels.push_back(texel);
 						break;
-					default: // position
+
+					default:
 						float3 position;
 						position.x = strtof(tokens[1], nullptr);
 						position.y = strtof(tokens[2], nullptr);
 						position.z = strtof(tokens[3], nullptr);
-						rawMeshData.Positions.push_back(position);
+						result.Positions.push_back(position);
 						break;
 				}
 				break;
 			}
-			case 'f': // face
+
+			case 'f':
 			{
 				// split line into tokens
 				while (token != NULL)
@@ -105,18 +189,17 @@ OBJMesh OBJMeshLoader::LoadOBJMesh(const char* filePath)
 					tokens.push_back(token);
 					token = strtok_s(NULL, separators, &nextToken);
 				}
-				std::vector<uint3> tokenVertices;
+				vector<UnstructuredVertex> tokenVertices;
 				char faceSeparators[] = "/";
 
 				// convert tokens into vertices
 				for (unsigned int i = 1; i < tokens.size(); ++i)
 				{
-					// copy token to split further
 					char extractFacesFrom[100];
 					strcpy_s(extractFacesFrom, tokens[i]);
 
 					// split token into individual values
-					std::vector<char*> faceTokens;
+					vector<char*> faceTokens;
 					token = strtok_s(extractFacesFrom, faceSeparators, &nextToken);
 					while (token != NULL)
 					{
@@ -124,106 +207,51 @@ OBJMesh OBJMeshLoader::LoadOBJMesh(const char* filePath)
 						token = strtok_s(NULL, faceSeparators, &nextToken);
 					}
 
-					// store values and add to list
-					// raw values are 1-based, need to subtract 1 from each to make them 0-based
-					uint3 vertex;
-					vertex.x = strtoul(faceTokens[0], nullptr, 10) - 1;
-					vertex.y = strtoul(faceTokens[1], nullptr, 10) - 1;
-					vertex.z = strtoul(faceTokens[2], nullptr, 10) - 1;
+					// convert values and add to list; raw values are 1-based, need to subtract 1 from each to make them 0-based
+					UnstructuredVertex vertex;
+					vertex.PositionIndex = strtoul(faceTokens[0], nullptr, 10) - 1;
+					vertex.TexelIndex = strtoul(faceTokens[1], nullptr, 10) - 1;
+					vertex.NormalIndex = strtoul(faceTokens[2], nullptr, 10) - 1;
 					tokenVertices.push_back(vertex);
 				}
 
 				// assemble faces from vertex list, dividing quads into triangles if necessary
 				OBJTriangle face = {};
-				face.VertexIndices[0] = tokenVertices[0];
-				face.VertexIndices[1] = tokenVertices[1];
-				face.VertexIndices[2] = tokenVertices[2];
-				rawMeshData.Faces.push_back(face);
+				face.Vertices[0] = tokenVertices[0];
+				face.Vertices[1] = tokenVertices[1];
+				face.Vertices[2] = tokenVertices[2];
+				result.Faces.push_back(face);
 				if (tokens.size() > 4)
 				{
-					face.VertexIndices[0] = tokenVertices[2];
-					face.VertexIndices[1] = tokenVertices[3];
-					face.VertexIndices[2] = tokenVertices[0];
-					rawMeshData.Faces.push_back(face);
+					face.Vertices[0] = tokenVertices[2];
+					face.Vertices[1] = tokenVertices[3];
+					face.Vertices[2] = tokenVertices[0];
+					result.Faces.push_back(face);
 				}
 				break;
 			}
+
 			default:
 				break;
 		}
 	}
+
 	inputFileStream.close();
-	// ----- PROCESS RAW DATA INTO LISTS -----
+	return result;
+}
 
-	// ----- CONVERT LISTS INTO VERT / IND DATA -----
-	std::vector<uint3> rawVertexData;
-	std::vector<unsigned int> indexList;
-	// iterate through faces
-	for (unsigned int faceIndex = 0; faceIndex < rawMeshData.Faces.size(); faceIndex++)
-	{
-		// iterate through face's vertices
-		for (unsigned int faceVertexIndex = 0; faceVertexIndex < 3; faceVertexIndex++)
-		{
-			bool isUnique = true;
-			unsigned int index = 0;
-			// iterate through vertex list
-			for (unsigned int vertexListIndex = 0; vertexListIndex < rawVertexData.size(); vertexListIndex++)
-			{
-				// check vert values against current list item values
-				if (rawMeshData.Faces[faceIndex].VertexIndices[faceVertexIndex].x == rawVertexData[vertexListIndex].x
-					&& rawMeshData.Faces[faceIndex].VertexIndices[faceVertexIndex].y == rawVertexData[vertexListIndex].y
-					&& rawMeshData.Faces[faceIndex].VertexIndices[faceVertexIndex].z == rawVertexData[vertexListIndex].z)
-				{
-					// vertex is duplicate, store index where it was found
-					isUnique = false;
-					index = vertexListIndex;
-					break;
-				}
-			}
-			// if vert is unique, add to list and store index where it was added
-			if (isUnique)
-			{
-				index = rawVertexData.size();
-				rawVertexData.push_back(rawMeshData.Faces[faceIndex].VertexIndices[faceVertexIndex]);
-			}
-			// add index of vert to list
-			indexList.push_back(index);
-		}
-	}
-	// ----- CONVERT LISTS INTO VERT / IND DATA -----
 
-	// ----- CONVERT VERT DATA INTO VERTS AND COPY INTO ARRAY -----
-	OBJVertex* vertices = new OBJVertex[rawVertexData.size()];
-	for (unsigned int i = 0; i < rawVertexData.size(); ++i)
+OBJMesh OBJMeshLoader::LoadOBJMesh(const char* filePath)
+{
+	UnstructuredMeshData unstructuredMeshData = ReadUnstructuredMeshDataFromFile(filePath);
+	CompactifiedMeshData compactifiedMeshData = CompactifyUnstructuredMeshData(unstructuredMeshData);
+	NormalizedMeshData normalizedMeshData = NormalizeStructuredMeshData(unstructuredMeshData, compactifiedMeshData);
+	OBJMesh result =
 	{
-		OBJVertex vertex = {};
-		vertex.Position.x = rawMeshData.Positions[rawVertexData[i].x].x;
-		vertex.Position.y = rawMeshData.Positions[rawVertexData[i].x].y;
-		vertex.Position.z = rawMeshData.Positions[rawVertexData[i].x].z;
-		vertex.Texel.x = rawMeshData.Texels[rawVertexData[i].y].x;
-		vertex.Texel.y = rawMeshData.Texels[rawVertexData[i].y].y;
-		vertex.Texel.z = rawMeshData.Texels[rawVertexData[i].y].z;
-		vertex.Normal.x = rawMeshData.Normals[rawVertexData[i].z].x;
-		vertex.Normal.y = rawMeshData.Normals[rawVertexData[i].z].y;
-		vertex.Normal.z = rawMeshData.Normals[rawVertexData[i].z].z;
-		vertices[i] = vertex;
-	}
-	// ----- CONVERT VERT DATA INTO VERTS AND COPY INTO ARRAY -----
-
-	// ----- COPY INDEX LIST INTO ARRAY -----
-	unsigned int* indices = new unsigned int[indexList.size()];
-	for (unsigned int i = 0; i < indexList.size(); ++i)
-	{
-		indices[i] = indexList[i];
-	}
-	// ----- COPY INDEX LIST INTO ARRAY -----
-
-	OBJMesh mesh =
-	{
-		vertices,
-		rawVertexData.size(),
-		indices,
-		indexList.size(),
+		normalizedMeshData.Vertices,
+		compactifiedMeshData.Vertices.size(),
+		normalizedMeshData.Indices,
+		compactifiedMeshData.Indices.size(),
 	};
-	return mesh;
+	return result;
 }
