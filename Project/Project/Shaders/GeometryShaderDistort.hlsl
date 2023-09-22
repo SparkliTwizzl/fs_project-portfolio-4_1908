336a4841a5774e32c68dff2c5659c72b73829c7f
Artemis K. Frost (she/her) <github@artemiskfrost.com>
2023-09-22 09:13:02 -0600

    condense geometry and vertex distort shaders, fix bug in vertex shader

--- Project/Project/Shaders/GeometryShaderDistort.hlsl
+++ Project/Project/Shaders/GeometryShaderDistort.hlsl
@@ -4,36 +4,20 @@
 [maxvertexcount(6)]
 void main(triangle ShaderVertex input[3], inout TriangleStream<ShaderVertex> outputStream)
 {
-	ShaderVertex output = (ShaderVertex) 0;
-
 	float3 offsets[2] =
 	{
 		float3(-1, 0, 0),
-		float3(1, 0, 0)
+		float3(1, 0, 0),
 	};
 
+	ShaderVertex output = (ShaderVertex) 0;
 	for (uint i = 0; i < 6; ++i)
 	{
-		if (i < 3)
-		{
-			output.Color = input[i].Color;
-			output.InstanceId = input[i].InstanceId;
-			output.Normal = input[i].Normal;
-			output.Position = input[i].Position + float4(offsets[0], 0);
-			output.Texel = input[i].Texel;
-			output.WorldPosition = input[i].WorldPosition + float4(offsets[0], 0);
-		}
-		else
-		{
-			uint index = i - 3;
-			output.Color = input[index].Color;
-			output.InstanceId = input[index].InstanceId;
-			output.Normal = input[index].Normal;
-			output.Position = input[index].Position + float4(offsets[1], 0);
-			output.Texel = input[index].Texel;
-			output.WorldPosition = input[index].WorldPosition + float4(offsets[1], 0);
-		}
-
+		uint vertexIndex = i % 3;
+		uint offsetIndex = i / 3;
+		output = input[vertexIndex];
+		output.Position += float4(offsets[offsetIndex], 0);
+		output.WorldPosition += float4(offsets[offsetIndex], 0);
 		outputStream.Append(output);
 	}
 
--- Project/Project/Shaders/VertexShaderDistort.hlsl
+++ Project/Project/Shaders/VertexShaderDistort.hlsl
@@ -4,22 +4,17 @@
 ShaderVertex main(RawVertex input)
 {
 	ShaderVertex output = (ShaderVertex) 0;
+	float4 distortedPosition = float4(sin(input.Position.x * Time), sin(input.Position.y * Time), sin(input.Position.z * Time), input.Position.w);
 
 	output.Color = input.Color;
 	output.InstanceId = input.InstanceId;
 	output.Normal = mul(float4(input.Normal, 0), WorldMatrix).xyz;
-	output.Position.x = sin(input.Position.x * Time);
-	output.Position.y = sin(input.Position.y * Time);
-	output.Position.z = sin(input.Position.z * Time);
-	output.Position = mul(output.Position, InstanceOffsets[input.InstanceId]);
+	output.Position = mul(distortedPosition, InstanceOffsets[input.InstanceId]);
 	output.Position = mul(output.Position, WorldMatrix);
 	output.Position = mul(output.Position, ViewMatrix);
 	output.Position = mul(output.Position, ProjectionMatrix);
 	output.Texel = input.Texel;
-	output.WorldPosition.x = sin(input.Position.x * Time);
-	output.WorldPosition.y = sin(input.Position.y * Time);
-	output.WorldPosition.z = sin(input.Position.z * Time);
-	output.WorldPosition = mul(output.WorldPosition, InstanceOffsets[input.InstanceId]);
+	output.WorldPosition = mul(distortedPosition, InstanceOffsets[input.InstanceId]);
 	output.WorldPosition = mul(output.WorldPosition, WorldMatrix);
 
 	return output;
